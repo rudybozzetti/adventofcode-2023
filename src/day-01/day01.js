066019966ca62602bdcb5bce345579099d54b368
rudy <rudy.bozzetti@gmail.com>
2023-12-02 19:34:42 +0100

    feat: day 01 part 2

--- src/day-01/day01.js
+++ src/day-01/day01.js
@@ -1,8 +1,50 @@
 
-export const parseInput = data => {
+export const parseInput = (data) => {
   return data.split(/\n/).reduce((acc, line, row) => {
     const numbers = line.match(/\d/g)
     const n = parseInt(`${numbers[0]}${numbers[numbers.length - 1]}`)
+    return [...acc, n]
+
+  }, [])
+}
+
+export const convToNumber = s => {
+  switch (s) {
+    case 'one':
+      return 1
+    case 'two':
+      return 2
+    case 'three':
+      return 3
+    case 'four':
+      return 4
+    case 'five':
+      return 5
+    case 'six':
+      return 6
+    case 'seven':
+      return 7
+    case 'eight':
+      return 8
+    case 'nine':
+      return 9
+    default:
+      return s
+  }
+}
+
+export const tokens = [
+  '1', '2', '3', '4', '5', '6', '7', '8', '9',
+  'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'
+]
+
+export const parseInput2 = (data) => {
+  return data.split(/\n/).reduce((acc, line, row) => {
+    const firstMatches = tokens.map(t => [t, line.indexOf(t)]).filter(n => n[1] !== -1).sort((a, b) => a[1] - b[1])
+    const lastMatches = tokens.map(t => [t, line.lastIndexOf(t)]).filter(n => n[1] !== -1).sort((a, b) => b[1] - a[1])
+
+    const n = parseInt(`${convToNumber(firstMatches[0][0])}${convToNumber(lastMatches[0][0])}`)
+
     return [...acc, n]
 
   }, [])
@@ -15,7 +57,8 @@ export const part1 = (inputData) => {
 
 }
 
+export const part2 = (inputData) => {
+  const list = parseInput2(inputData)
 
-export const part2 = () => {
-
+  return list.reduce((acc, n) => acc + n, 0)
 }
--- src/day-01/day01.test.js
+++ src/day-01/day01.test.js
@@ -1,10 +1,18 @@
 
-import { part1 } from './day01'
+import { part1, part2, convToNumber } from './day01'
 
 const testInput = `1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet`
+
+const testInput2 = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`
 
 describe('day01', () => {
 
@@ -12,6 +20,51 @@ describe('day01', () => {
   it('part1', () => {
 
     expect(part1(testInput)).toBe(142)
+
+  })
+
+  it('convToNumber', () => {
+    expect(convToNumber('one')).toBe(1)
+    expect(convToNumber('two')).toBe(2)
+    expect(convToNumber('three')).toBe(3)
+    expect(convToNumber('four')).toBe(4)
+    expect(convToNumber('five')).toBe(5)
+    expect(convToNumber('six')).toBe(6)
+    expect(convToNumber('seven')).toBe(7)
+    expect(convToNumber('eight')).toBe(8)
+    expect(convToNumber('nine')).toBe(9)
+    expect(convToNumber('1')).toBe('1')
+  })
+
+  it.only('part2 tricky', () => {
+    expect(part2('fiveight')).toBe(58)
+    expect(part2('one7one')).toBe(11)
+
+    expect(part2('oneight')).toBe(18)
+    expect(part2('oneightwo')).toBe(12)
+    expect(part2('twone')).toBe(21)
+    expect(part2('eightwo')).toBe(82)
+    expect(part2('two')).toBe(22)
+    expect(part2('one')).toBe(11)
+    expect(part2('one7one')).toBe(11)
+
+    expect(part2('4231')).toBe(41)
+    expect(part2('3oneone')).toBe(31)
+    expect(part2('zmtdfour8')).toBe(48)
+    expect(part2('797ninetwotwo')).toBe(72)
+
+
+
+
+
+
+
+
+  })
+
+  it('part2', () => {
+
+    expect(part2(testInput2)).toBe(281)
 
   })
 
